b290fb21f185842a56967075073631f80e7b55b4
unduli-attacked <45951442+unduli-attacked@users.noreply.github.com>
2019-03-13 17:16:01 -0700

    get rid of pickuphatch
    its kinda useless without vision

--- src/main/java/frc/robot/commands/HelperRocketAuto.java
+++ src/main/java/frc/robot/commands/HelperRocketAuto.java
@@ -5,6 +5,16 @@ import edu.wpi.first.wpilibj.command.Command;
 public class HelperRocketAuto extends Command {
 	public HelperRocketAuto() {
 
+		/*
+		Thing do:
+			Drive from starting location to the farside left rocket (I think, opposite the one we're doing, right?)
+			Place hatch
+			Drive to opposite loading station from us
+			Pickup hatch
+			Drive to nearside rocket
+			Place hatch
+		*/
+
 	}
 
 	@Override
